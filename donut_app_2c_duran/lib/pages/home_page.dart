6ae0781e1869aecdc98b1f4a68b6cb9e3b0621c4
karlosDuran <ferni0425@gmail.com>
2025-02-12 18:52:12 -0600

    Main Text added

--- donut_app_2c_duran/lib/pages/home_page.dart
+++ donut_app_2c_duran/lib/pages/home_page.dart
@@ -12,13 +12,45 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      backgroundColor: Colors.transparent,
-      //icono izquierdo
-      leading: Icon(Icons.menu, color: Colors.grey[800]),
-      //icono derecho
-      actions: [Icon(Icons.person)],
-    ));
+          backgroundColor: Colors.transparent,
+          //Ícono izquierdo
+          leading: Icon(Icons.menu, color: Colors.grey[800]),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        body: const Column(
+          children: [
+            //Texto principal
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+              child: Row(
+                children: [
+                  Text(
+                    "I want to ",
+                    style: TextStyle(fontSize: 32),
+                  ),
+                  Text("Eat",
+                      style: TextStyle(
+                          //Tamaño de letra
+                          fontSize: 32,
+                          //Negritas
+                          fontWeight: FontWeight.bold,
+                          //Subrayado
+                          decoration: TextDecoration.underline))
+                ],
+              ),
+            ),
+
+            //Pestañas (TabBar)
+
+            //Contenido de pestañas (TabBarView)
+
+            //Carrito (Cart)
+          ],
+        ));
   }
 }
-//que es el widget scaffold, widget de estructura basica
-//que es estado de aplicacion
